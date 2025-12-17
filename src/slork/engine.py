2b8fe06bb193c3663261fb393087685e099f041a
Tom Mulgrew <tmulgrew@slingshot.co.nz>
2025-12-17 22:02:53 +1300

    Implement taking items

--- src/slork/engine.py
+++ src/slork/engine.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from .commands import ParsedCommand
-from .world import World, Exit
+from .world import World, Item
 
 @dataclass
 class GameState:
@@ -27,13 +27,12 @@ def describe_current_location(state: GameState) -> str:
     lines = [location.name, location.description]
 
     # Items
-    if location.items:
-        item_descriptions = []
-        for item_id in location.items:
-            item = state.world.items[item_id]
-            item_descriptions.append(item.name)
-        if item_descriptions:
-            lines.append(f"You see: {', '.join(item_descriptions)}")
+    item_descriptions = []
+    for item_id in (location.items or []):
+        item = state.world.items[item_id]
+        item_descriptions.append(item.name)
+    if item_descriptions:
+        lines.append(f"You see: {', '.join(item_descriptions)}")
 
     # Exits
     exit_descriptions = []
@@ -53,6 +52,8 @@ def handle_command(state: GameState, command: ParsedCommand) -> ActionResult:
         return ActionResult(status = "ok", message = describe_current_location(state))
     if command.verb == "go":
         return handle_go(state, command.object)
+    if command.verb == "take":
+        return handle_take(state, command.object)
     return ActionResult(status = "no_effect", message="That didn't work.")
 
 def handle_go(state: GameState, direction: str) -> ActionResult:
@@ -73,3 +74,36 @@ def handle_go(state: GameState, direction: str) -> ActionResult:
 
 def has_required_flags(state: GameState, required_flags) -> bool:
     return all(flag in state.flags for flag in (required_flags or []))
+
+def handle_take(state: GameState, object: object) -> ActionResult:
+
+    # Find matching items at location
+    location = state.world.locations[state.location_id]
+    matches = [ 
+        item_id
+        for item_id in (location.items or [])
+        if item_matches_object(state.world.items[item_id], object)
+    ]
+
+    # Must be exactly one
+    if not matches:
+        return ActionResult(status = "invalid", message = f"There is no {object} here.")
+    
+    if len(matches) > 1:
+        return ActionResult(status = "invalid", message = f"Which {object}?")
+
+    item_id = matches[0]
+    item = state.world.items[item_id]
+
+    # Item must be portable
+    if not item.portable:
+        return ActionResult(status = "no_effect", message = f"The {item.name} cannot be taken.")
+    
+    # Remove from location and add to inventory
+    state.inventory.append(item_id)
+    location.items.remove(item_id)
+
+    return ActionResult(status = "ok", message = f"You took the {item.name}.")               
+
+def item_matches_object(item: Item, object: str):
+    return item.name.lower() == object or object in (item.aliases or [])
