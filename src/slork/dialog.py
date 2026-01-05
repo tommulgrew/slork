132d2b3ee2bd62616ee6519771097370784c54f6
Tom Mulgrew <tmulgrew@slingshot.co.nz>
2026-01-05 19:10:03 +1300

    Add dialog effect to data structure

--- src/slork/dialog.py
+++ src/slork/dialog.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Optional
-from .logic import Criteria, ResolvableText
+from .logic import Criteria, Effect, ResolvableText
 
 @dataclass
 class DialogTree:
     npc_narrative: ResolvableText
     player_narrative: Optional[ResolvableText] = None
     criteria: Optional[Criteria] = None
+    effect: Optional[Effect] = None
     aliases: list[str] = field(default_factory=list)                            # Aliases for main keyword
     responses: dict[str, DialogTree] = field(default_factory=dict)              # Keyed by keyword.
