f4812d0e48fdf5508ba551ef16a761a0cefe9815
Tom Mulgrew <tmulgrew@slingshot.co.nz>
2026-01-05 16:36:38 +1300

    Refactor dialog for better readable yaml

--- src/slork/dialog.py
+++ src/slork/dialog.py
@@ -4,14 +4,24 @@ from typing import Optional
 from .conditions import Criteria, ResolvableText
 
 @dataclass
-class DialogOption:
-    keyword: str
-    subtree: DialogTree
-    player_narrative: ResolvableText
-    criteria: Optional[Criteria] = None
-    keyword_aliases: Optional[list[str]] = field(default_factory=list)
-
-@dataclass
 class DialogTree:
     npc_narrative: ResolvableText
-    options: list[DialogOption] = field(default_factory=list)
+    player_narrative: Optional[ResolvableText] = None
+    criteria: Optional[Criteria] = None
+    aliases: list[str] = field(default_factory=list)                    # Aliases for main keyword
+    responses: dict[str, DialogTree] = field(default_factory=dict)      # Grouped by keyword
+
+# dialog:
+#   npc_narrative: What brings you to this place?
+#   responses:
+#       dragons:
+#           player_narrative: You lean in conspiratorially. "I've heard there are dragons in these parts."
+#           npc_narrative: I see. And what particular color dragons would you be looking for?
+#           responses:
+#               blue:
+#                   ...
+#               golden:
+#                   ...
+#       "no reason":
+#           player_narrative: I'd prefer not to say.
+
