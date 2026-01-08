e6023decc61e38c6c0abab033329a58b83924826
Tom Mulgrew <tmulgrew@slingshot.co.nz>
2026-01-08 13:26:17 +1300

    Tidy up DialogTree class

--- src/slork/dialog.py
+++ src/slork/dialog.py
@@ -5,12 +5,12 @@ from .logic import Criteria, Effect, ResolvableText
 
 @dataclass
 class DialogTree:
-    jump_target: Optional[str]                                                  # Unique ID for jumps
-    npc_narrative: Optional[ResolvableText]
-    player_narrative: Optional[ResolvableText] = None
-    criteria: Optional[Criteria] = None
-    effect: Optional[Effect] = None
+    criteria: Optional[Criteria] = None                                         # Dialog subtree is only available if criteria is satisfied (not meaningful for root nodes)
     aliases: list[str] = field(default_factory=list)                            # Aliases for main keyword
-    responses: dict[str, DialogTree] = field(default_factory=dict)              # Keyed by keyword.
-    keyword_hint: Optional[str] = None
+    keyword_hint: Optional[str] = None                                          # Displayed to player when in parent node, to hint at the direction the dialog will take if this node is selected.
+    jump_target: Optional[str] = None                                           # Unique ID for jumping to this node (see 'jump')
+    player_narrative: Optional[ResolvableText] = None                           # Player narrative text to display 
+    npc_narrative: Optional[ResolvableText] = None                              # NPC narrative text to display 
+    effect: Optional[Effect] = None                                             # Effect to apply
     jump: Optional[ResolvableText] = None                                       # Used to jump to another node
+    responses: dict[str, DialogTree] = field(default_factory=dict)              # Subtrees to execute based on player's selected response
